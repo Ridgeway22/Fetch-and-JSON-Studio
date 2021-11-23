6d4c50154397ac81dabf09a2b7273943b6d6fcc9
Ridgeway22 <boo22mickey@yahoo.com>
2021-11-22 21:07:26 -0600

    finish studio

--- script.js
+++ script.js
@@ -3,25 +3,47 @@ window.addEventListener("load", function() {
     const fetchPromise = fetch("https://handlers.education.launchcode.org/static/astronauts.json");
     fetchPromise.then( function(response) {
         const jsonPromise = response.json();
-        let i = 0;
+        
         jsonPromise.then( function(json) {
-           
+            let i = 0;
             let div = document.getElementById('container');
+                const count = document.getElementById("count");
+             count.innerHTML = `Number of Astronauts ${json.length}`;
+            let divs;
            json.map(function(a){
+               const organize = json.sort((a,b)=>
+                   b.hoursInSpace - a.hoursInSpace);
+               console.log(organize);
           for (;i< json.length; i++){
-              const divs = `
+              if (json[i].active){
+              divs = `
                 <div class="astronaut">
                     <div class="bio">
                         <h3>${json[i].firstName} ${json[i].lastName}</h3>
                             <ul>
                                 <li>Hours in space:  ${json[i].hoursInSpace}</li>
-                                <li>Active: ${json[i].active}</li>
+                                <li style='color: green' >Active: ${json[i].active}</li>
                                 <li>Skills ${json[i].skills}</li>
                             </ul>
-                            <img class="avatar" src=${json[i].picture}/>
+                            <img class="avatar" src=${json[i].picture}>
                     </div>
-                </div>`;
-                div.insertAdjacentHTML("beforeend", divs);}
+                </div>`;}
+                else {
+                    divs = `
+                      <div class="astronaut">
+                          <div class="bio">
+                              <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                                  <ul>
+                                      <li>Hours in space:  ${json[i].hoursInSpace}</li>
+                                      <li>Active: ${json[i].active}</li>
+                                      <li>Skills ${json[i].skills}</li>
+                                  </ul>
+                                  <img class="avatar" src=${json[i].picture}>
+                          </div>
+                      </div>`;}
+               
+                div.insertAdjacentHTML("beforeend", divs);
+            }
             });
           
            
